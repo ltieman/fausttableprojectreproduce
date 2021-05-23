edf1af10e13a99363648755bd764b45549f0589e
lucas.tieman <lucas.tieman@selui.org>
2021-05-23 16:28:19 -0500

    code to reproduce issue with table based off hello world

---
+++ main.py
@@ -0,0 +1,18 @@
+import faust
+
+app = faust.App(
+    'hello-world',
+    broker='kafka://localhost:9092',
+    value_serializer='raw',
+)
+
+greetings_topic = app.topic('greetings')
+table = app.Table('something', partitions=1)
+
+@app.agent(greetings_topic)
+async def greet(greetings):
+    async for greeting in greetings:
+        print(greeting)
+        table[greeting] = greeting
+
+
